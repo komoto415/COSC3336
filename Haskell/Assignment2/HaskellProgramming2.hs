9ddf3c9a1187178e6a17a09b8bee0283aac335d3
komoto415 <komoto415@gmail.com>
2019-11-12 08:21:31 -0600

    sure

--- Haskell/Assignment2/HaskellProgramming2.hs
+++ Haskell/Assignment2/HaskellProgramming2.hs
@@ -9,9 +9,9 @@ eq x y = x == y
 gr x y = x  > y
 ls x y = x  < y
 
-func :: (t -> t -> Bool) -> List t -> t -> List Bool
-func f (C x xs) n = C (f x n) (func f xs n)
-func f E n        = E
+function :: (t -> t -> Bool) -> [t] -> t -> [Bool]
+function f [] n = []
+function f (x:xs) n = (f x n) : (function f xs n)
 
 bubsort :: (Ord a)  => [a] -> [a]
 bubsort []          = []
@@ -32,11 +32,11 @@ print(llastTwo([6,5,4,3,2,1]))
 print(llastTwo([12,13,54,124,1241,312]))
 print(llastTwo([1]))
 
-let arr1 = C 1 (C 2 (C 3 E))
+let arr1 = [1,2,3,4,5,6]
 
-print(func eq arr1 2) -- Should print [False,True,False,False,False,False,False]
-print(func gr arr1 2) -- Should print [False,False,True,True,True,True,True]
-print(func ls arr1 2) -- Should print [True,False,False,False,False,False,False]
+print(function eq arr1 2) -- Should print [False,True,False,False,False,False,False]
+print(function gr arr1 2) -- Should print [False,False,True,True,True,True,True]
+print(function ls arr1 2) -- Should print [True,False,False,False,False,False,False]
 
 let arr2 = [1,5,2,10,9,-3,15]
 
